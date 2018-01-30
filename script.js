7faaaca409c5be29873cf1872bcf704df0f81b34
MarioAntony <mario1995304@gmail.com>
2018-01-31 00:04:10 +0530

    Sound_cloud

--- script.js
+++ script.js
@@ -6,11 +6,124 @@ SC.initialize({
 $(document).ready(function() {
 
   SC.stream('/tracks/155366202', function(sound) {
-    $('#start12').click(function(e) {
+    $('#start').click(function(e) {
       e.preventDefault();
       sound.start();
     });
-    $('#stop12').click(function(e) {
+    $('#stop').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  SC.stream('/tracks/173826641', function(sound) {
+    $('#start1').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop1').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+  SC.stream('/tracks/335350731', function(sound) {
+    $('#start2').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop2').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+  SC.stream('/tracks/349414693', function(sound) {
+    $('#start3').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop3').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+  SC.stream('/tracks/354663899', function(sound) {
+    $('#start4').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop4').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+  SC.stream('/tracks/316859203', function(sound) {
+    $('#start5').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop5').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+  SC.stream('/tracks/27107039', function(sound) {
+    $('#start6').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop6').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+  SC.stream('/tracks/317969885', function(sound) {
+    $('#start7').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop7').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+  SC.stream('/tracks/339606057', function(sound) {
+    $('#start8').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop8').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+  SC.stream('/tracks/235717376', function(sound) {
+    $('#start9').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop9').click(function(e) {
       e.preventDefault();
       sound.stop();
     });
