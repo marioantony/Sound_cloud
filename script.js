4106524742bfe88fe91d25429cd99169c559b5fd
MarioAntony <mario1995304@gmail.com>
2018-02-01 00:00:24 +0530

    correction of soundcloud

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ SC.initialize({
 
 $(document).ready(function() {
 
+  SC.stream('/tracks/155366202', function(sound) {
+    $('#start').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
 
-  SC.stream('/tracks/339606057', function(sound) {
+  SC.stream('/tracks/173826641', function(sound) {
     $('#start1').click(function(e) {
       e.preventDefault();
       sound.start();
@@ -18,7 +28,7 @@ $(document).ready(function() {
   });
 
 
-  SC.stream('/tracks/316972940', function(sound) {
+  SC.stream('/tracks/335350731', function(sound) {
     $('#start2').click(function(e) {
       e.preventDefault();
       sound.start();
@@ -31,7 +41,7 @@ $(document).ready(function() {
 
 
 
-  SC.stream('/tracks/273757802', function(sound) {
+  SC.stream('/tracks/349414693', function(sound) {
     $('#start3').click(function(e) {
       e.preventDefault();
       sound.start();
@@ -44,7 +54,7 @@ $(document).ready(function() {
 
 
 
-  SC.stream('/tracks/95378510', function(sound) {
+  SC.stream('/tracks/354663899', function(sound) {
     $('#start4').click(function(e) {
       e.preventDefault();
       sound.start();
@@ -57,7 +67,7 @@ $(document).ready(function() {
 
 
 
-  SC.stream('/tracks/317968951', function(sound) {
+  SC.stream('/tracks/316859203', function(sound) {
     $('#start5').click(function(e) {
       e.preventDefault();
       sound.start();
@@ -70,7 +80,7 @@ $(document).ready(function() {
 
 
 
-  SC.stream('/tracks/298956840', function(sound) {
+  SC.stream('/tracks/27107039', function(sound) {
     $('#start6').click(function(e) {
       e.preventDefault();
       sound.start();
@@ -83,7 +93,7 @@ $(document).ready(function() {
 
 
 
-  SC.stream('/tracks/254362918', function(sound) {
+  SC.stream('/tracks/317969885', function(sound) {
     $('#start7').click(function(e) {
       e.preventDefault();
       sound.start();
